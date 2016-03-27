9f44ef2fc4e935a7a2fea8fc8dd941000e038658
Norman <el.norman01@gmail.com>
2016-03-27 18:20:50 -0400

    Se agrega soporte para placas de exhibición

--- LectorQRv2/Models/Placa.cs
+++ LectorQRv2/Models/Placa.cs
@@ -10,7 +10,7 @@ namespace LectorQRv2.Models
 
         public Placa(string serie)
         {
-            string formato = @"^[AGLF]{1}\d{6}";
+            string formato = @"^[AGLFX]{1}\d{6}";
             Regex regex = new Regex(formato);
 
             if (regex.IsMatch(serie.ToUpper()))
